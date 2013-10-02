fdb19db09175218a22cbc8f24cf9636abaac096b
Chris Karr <github@audacious-software.com>
2013-10-02 13:55:13 -0500

    Player activity work

--- Purple Chill/src/edu/northwestern/cbits/intellicare/relax/PlayerActivity.java
+++ Purple Chill/src/edu/northwestern/cbits/intellicare/relax/PlayerActivity.java
@@ -1,13 +1,25 @@
 package edu.northwestern.cbits.intellicare.relax;
 
+import java.util.ArrayList;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class PlayerActivity extends ActionBarActivity 
 {
@@ -23,6 +35,51 @@ public class PlayerActivity extends ActionBarActivity
 
 		this.setContentView(R.layout.activity_player);
 		this.getSupportActionBar().setTitle(this._groupName);
+		
+		final ArrayList<String> recordingNames = new ArrayList<String>();
+		
+		for (int i = 0; i < 16; i++)
+		{
+			recordingNames.add(i + " Test Recording (12:34)");
+		}
+		
+		final PlayerActivity me = this;
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.row_recording, recordingNames)
+		{
+			public View getView(int position, View convertView, ViewGroup parent)
+			{
+				if (convertView == null)
+				{
+					LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+					convertView = inflater.inflate(R.layout.row_recording, parent, false);
+				}
+				
+				TextView title = (TextView) convertView.findViewById(R.id.recording_title);
+				title.setText(recordingNames.get(position));
+				
+				Drawable d = me.getResources().getDrawable(R.drawable.ic_action_music_2);
+				
+				if (position == 3)
+					d = me.getResources().getDrawable(R.drawable.ic_action_playback_play);
+				
+				title.setCompoundDrawablesWithIntrinsicBounds(d, null, null, null);
+				
+				return convertView;
+			}
+		};
+		
+		ListView recordingsList = (ListView) this.findViewById(R.id.recording_list);
+		
+		recordingsList.setAdapter(adapter);
+		
+		recordingsList.setOnItemClickListener(new OnItemClickListener()
+		{
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
+			{
+				Log.e("PC", "TAPPED " + position);
+			}
+		});
 	}
 	
 	protected void onResume()
@@ -32,8 +89,6 @@ public class PlayerActivity extends ActionBarActivity
 		String completedKey = this._groupName + "_completed";
 		
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		
-		Log.e("PC", "EVALING KEY " + completedKey);
 		
 		if (prefs.getBoolean(completedKey, false) == false)
 		{
