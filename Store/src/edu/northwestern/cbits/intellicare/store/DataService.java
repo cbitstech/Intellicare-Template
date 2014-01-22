2683619e2272a657b1c0f8995ca44c549a946a95
Evan Story <estory1@gmail.com>
2014-01-22 17:56:51 -0600

    Main UI layout defined. Trying to fill the UI elements with data from a data source (local JSON files for now), but currently crashing.

---
+++ Store/src/edu/northwestern/cbits/intellicare/store/DataService.java
@@ -0,0 +1,56 @@
+package edu.northwestern.cbits.intellicare.store;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+
+public class DataService {
+	
+//	private enum JSONTypes {
+//		STR, INT, DBL, OBJ, ARR, DATETIME
+//	}
+	
+	
+	public String getJsonFromFile(Context ctx, String filePath)
+	{
+		String jsonText = "";
+
+		BufferedReader reader = null;
+		try {
+			AssetManager am = ctx.getAssets();
+			InputStream is = am.open(filePath);
+			int size = is.available();
+			byte[] buffer = new byte[size];
+			is.read(buffer);
+			is.close();
+			jsonText = new String(buffer, "UTF-8");
+		}
+		catch(Exception e) {
+			Log.e("Store.DataService", "Couldn't read input file: " + filePath, e);
+		}
+		
+		return jsonText;
+	}
+	
+	public <T> T getJSONvalue(String pathInJsonObj, String jsonText)		// JSONTypes valueType, 
+	{
+		T ret = null;
+		try {
+			JSONArray arr = new JSONArray(jsonText);
+			JSONObject obj = arr.getJSONObject(0);
+			ret = (T) obj.getString(pathInJsonObj);
+			return ret;	
+		}
+		catch (Exception e) {
+			Log.e("Store.DataService", "Couldn't get JSON value at: " + pathInJsonObj, e);
+		}
+		return ret;
+	}
+}
--- Store/src/edu/northwestern/cbits/intellicare/store/MainActivity.java
+++ Store/src/edu/northwestern/cbits/intellicare/store/MainActivity.java
@@ -1,7 +1,12 @@
 package edu.northwestern.cbits.intellicare.store;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +15,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		DataService ds = new DataService();
+		String productsJsonText = ds.getJsonFromFile(this, "products.json");
+		String profileJsonText = ds.getJsonFromFile(this, "playerProfile.example.json");
+		
+		int userId = ds.getJSONvalue("userId", profileJsonText);
+		
+		Log.d("MainActivity.onCreate", "userId = " + userId);
 	}
 
 	@Override
