2691e2f655c7592e602deb46fdc683f66e339df6
Chris Karr <github@audacious-software.com>
2014-01-22 17:02:32 -0600

    Clock work

--- Slumber Time/src/edu/northwestern/cbits/intellicare/slumbertime/ClockActivity.java
+++ Slumber Time/src/edu/northwestern/cbits/intellicare/slumbertime/ClockActivity.java
@@ -7,8 +7,10 @@ import java.util.Date;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.content.res.Configuration;
 import android.database.Cursor;
 import android.os.Build;
@@ -16,19 +18,28 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.provider.CalendarContract;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.LayoutInflater;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 public class ClockActivity extends Activity 
 {
+	protected static final String ACTIVE_BRIGHTNESS_OPTION = "active_brightness_level";
+	protected static final String REST_BRIGHTNESS_OPTION = "rest_brightness_level";
+	protected static final float DEFAULT_ACTIVE_BRIGHTNESS = 1.0f;
+	protected static final float DEFAULT_REST_BRIGHTNESS = 0.25f;
+	
 	private Handler _handler = null;
 	private long _lastEventQuery = 0;
 	private Event _lastEvent = null;
+	protected float _currentBrightness;
 	
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 	public void onCreate(Bundle savedInstanceState) 
@@ -111,9 +122,134 @@ public class ClockActivity extends Activity
 				LayoutInflater inflater = LayoutInflater.from(me);
 				View view = inflater.inflate(R.layout.view_clock_settings, null, false);
 				
-				builder = builder.setView(view);
+				final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(me);
+
+				SeekBar activeBrightness = (SeekBar) view.findViewById(R.id.active_brightness);
+				activeBrightness.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
+				{
+					public void onProgressChanged(SeekBar bar, int position, boolean fromUser) 
+					{
+						WindowManager.LayoutParams params = me.getWindow().getAttributes();
+
+						params.screenBrightness = ((float) position) / 100f;
+
+						me.getWindow().setAttributes(params);
+					}
+
+					public void onStartTrackingTouch(SeekBar bar) 
+					{
+						WindowManager.LayoutParams params = me.getWindow().getAttributes();
+						
+						me._currentBrightness = params.screenBrightness;
+					}
+
+					public void onStopTrackingTouch(SeekBar bar)
+					{
+						WindowManager.LayoutParams params = me.getWindow().getAttributes();
+						params.screenBrightness = me._currentBrightness;
+						
+						me.getWindow().setAttributes(params);
+						
+						Editor e = prefs.edit();
+						e.putFloat(ClockActivity.ACTIVE_BRIGHTNESS_OPTION, ((float) bar.getProgress()) / 100f);
+						e.commit();
+					}
+				});
 				
-				builder.create().show();
+				float defaultActiveBrightness = prefs.getFloat(ClockActivity.ACTIVE_BRIGHTNESS_OPTION, ClockActivity.DEFAULT_ACTIVE_BRIGHTNESS);
+				activeBrightness.setProgress((int) (((float) activeBrightness.getMax()) * defaultActiveBrightness));
+
+				SeekBar restBrightness = (SeekBar) view.findViewById(R.id.rest_brightness);
+				restBrightness.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
+				{
+					public void onProgressChanged(SeekBar bar, int position, boolean fromUser) 
+					{
+						WindowManager.LayoutParams params = me.getWindow().getAttributes();
+
+						params.screenBrightness = ((float) position) / 100f;
+
+						me.getWindow().setAttributes(params);
+					}
+
+					public void onStartTrackingTouch(SeekBar bar) 
+					{
+						WindowManager.LayoutParams params = me.getWindow().getAttributes();
+						
+						me._currentBrightness = params.screenBrightness;
+					}
+
+					public void onStopTrackingTouch(SeekBar bar)
+					{
+						WindowManager.LayoutParams params = me.getWindow().getAttributes();
+						params.screenBrightness = me._currentBrightness;
+						
+						me.getWindow().setAttributes(params);
+						
+						SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(me);
+						Editor e = prefs.edit();
+						e.putFloat(ClockActivity.REST_BRIGHTNESS_OPTION, ((float) bar.getProgress()) / 100f);
+						e.commit();
+					}
+				});
+
+				float restActiveBrightness = prefs.getFloat(ClockActivity.REST_BRIGHTNESS_OPTION, ClockActivity.DEFAULT_REST_BRIGHTNESS);
+				restBrightness.setProgress((int) (((float) activeBrightness.getMax()) * restActiveBrightness));
+
+				final TextView dimLabel = (TextView) view.findViewById(R.id.dim_label);
+				
+				dimLabel.setText(me.getText(R.string.label_dim_delay, me.getTimeString(prefs.getInt(ClockActivity.DIM_DELAY_OPTION, ClockActivity.DEFAULT_DIM_DELAY))))
+				SeekBar dimDelay = (SeekBar) view.findViewById(R.id.dim_delay);
+				dimDelay.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
+				{
+					public void onProgressChanged(SeekBar bar, int position, boolean fromUser) 
+					{
+						
+					}
+
+					public void onStartTrackingTouch(SeekBar bar) 
+					{
+
+					}
+
+					public void onStopTrackingTouch(SeekBar bar)
+					{
+						WindowManager.LayoutParams params = me.getWindow().getAttributes();
+						params.screenBrightness = me._currentBrightness;
+						
+						me.getWindow().setAttributes(params);
+						
+						SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(me);
+						Editor e = prefs.edit();
+						e.putFloat(ClockActivity.REST_BRIGHTNESS_OPTION, ((float) bar.getProgress()) / 100f);
+						e.commit();
+					}
+				});
+
+				
+				builder = builder.setView(view);
+				builder = builder.setPositiveButton(R.string.button_close, new DialogInterface.OnClickListener() 
+				{
+					public void onClick(DialogInterface dialog, int which) 
+					{
+
+					}
+				});
+				
+				AlertDialog d = builder.create();
+				d.show();
+				
+				DisplayMetrics metrics = me.getResources().getDisplayMetrics();
+				
+				WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+
+				lp.copyFrom(d.getWindow().getAttributes());
+				lp.width = (int) (480f * metrics.density);
+
+				//change position of window on screen
+//				lp.x = mwidth/2; //set these values to what work for you; probably like I have here at
+//				lp.y = mheight/2;        //half the screen width and height so it is in center
+
+				d.getWindow().setAttributes(lp);
 			}
         });
     }
