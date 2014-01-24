b5877be8e64517e9efdeaa0b9179f3cfac558b80
Evan Story <estory1@gmail.com>
2014-01-24 17:35:57 -0600

    Refactoring.

--- Store/src/edu/northwestern/cbits/intellicare/store/MainActivity.java
+++ Store/src/edu/northwestern/cbits/intellicare/store/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		deleteAndRepopulateAllProducts();
+		SQLiteContentProvider.deleteAndRepopulateAllProducts(this);
 		
 		// query & display the data
 		Cursor c = this.getContentResolver().query(SQLiteContentProvider.StoreUri.PRODUCTS, null, null, null, null);
@@ -23,26 +23,6 @@ public class MainActivity extends Activity {
 		c.close();
 	}
 
-
-	/**
-	 * Clears and repopulates the Products table.
-	 */
-	private void deleteAndRepopulateAllProducts() {
-		// REFRESH ALL DB DATA: delete all data, then re-insert.
-		this.getContentResolver().delete(SQLiteContentProvider.StoreUri.PRODUCTS, null, null);
-		ContentValues values = new ContentValues();
-		for(Product i : ProductData.getAllProducts()) {
-			// TODO this is ugly and verbose; find a list+functional-ish way of dealing w/ the cols and values to insert.
-	        values.put(SQLiteContentProvider.Products.COLNM_USERHAZIT, 0);
-	        //values.put(SQLiteContentProvider.Products.COLNM_BLOB, "null");
-	        values.put(SQLiteContentProvider.Products.COLNM_NAME, i.name);
-	        values.put(SQLiteContentProvider.Products.COLNM_DESC, i.description);
-	        values.put(SQLiteContentProvider.Products.COLNM_ICON, i.icon);
-	        values.put(SQLiteContentProvider.Products.COLNM_ATTACHLOC, i.attachesOnAvatarAtLocation);
-	        values.put(SQLiteContentProvider.Products.COLNM_BUYPRICE, i.buyPrice);
-			this.getContentResolver().insert(SQLiteContentProvider.StoreUri.PRODUCTS, values);
-		}
-	}
 
 
 	@Override
