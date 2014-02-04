01627a3bf69db54dd358aadd43e1dd5eac473939
Evan Story <estory1@gmail.com>
2014-02-04 17:57:21 -0600

    Listing image links in a page works, either as relative or absolute URLs. More tests; currently working on testing fetch and save for multiple publicly-available URLs.

--- Mantra/src/edu/northwestern/cbits/intellicare/mantra/ImageExtractor.java
+++ Mantra/src/edu/northwestern/cbits/intellicare/mantra/ImageExtractor.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -52,22 +54,40 @@ public class ImageExtractor
 			.bodyAsBytes();
 		return imageResponse;
 	}
-	
+
 	/**
 	 * Returns a list of image URLs from some page at a specified URL.
 	 * Src: http://jsoup.org/cookbook/extracting-data/example-list-links
+	 * @param url
+	 * @param returnRelativePaths
 	 * @return
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	public static ArrayList<String> getImageList(String url) throws IOException {
+	public static ArrayList<String> getImageList(String url, boolean returnRelativePaths) throws IOException {
 		ArrayList<String> ret = new ArrayList<String>();
 		
 		Document doc = Jsoup.connect(url).get();
 		Elements links = doc.select("img");
-		
-		for (Element e : links) { ret.add(e.val()); }
+
+		// TODO: is there a less memory-intensive (e.g. Jsoup-internal) way to convert this to a standard Java datatype? 
+		for (Element e : links) { 
+			ret.add(
+				// if a relative path is requested, then return it...
+				returnRelativePaths
+				? e.attr("src")
+//						: e.attr("src").matches("(?i)https{0,1}://")
+//						: (REGEX_HTTP_HTTPS_URL_PROTO.matcher(e.attr("src"))).matches()
+						
+						// ...else, an absolute path is requested, so let's determine whether the image source already contains an absolute path, and return it if so, else prepend the base URL and return.
+						// no idea why regexes (dynamically-compiled and pre-compiled, respectively) above weren't working, but the following works, and should use fewer resources. 
+						: (e.attr("src").startsWith("http://") || e.attr("src").startsWith("https://") || e.attr("src").startsWith("HTTP://") || e.attr("src").startsWith("HTTPS://"))
+							? e.attr("src")
+							: e.baseUri() + e.attr("src") 
+				);
+		}
 		
 		return ret;
 	}
+//	private static final Pattern REGEX_HTTP_HTTPS_URL_PROTO = Pattern.compile("http", Pattern.CASE_INSENSITIVE);		// case-insensitive match of HTTP/HTTPS
 	
 }
--- Mantra/src/edu/northwestern/cbits/intellicare/mantra/tests/ImageExtractorTest.java
+++ Mantra/src/edu/northwestern/cbits/intellicare/mantra/tests/ImageExtractorTest.java
@@ -6,8 +6,12 @@ package edu.northwestern.cbits.intellicare.mantra.tests;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Connection.Response;
 
@@ -49,7 +53,7 @@ public class ImageExtractorTest extends TestCase {
 		String testMethodName = methodName();
 		String   baseUrl = "http://cbits.northwestern.edu/images/"
 				,fileName = "cbitsfulllogo.png"
-				,url = baseUrl + fileName;	// Yahoo logo
+				,url = baseUrl + fileName;
 		String 	outputFolder = "C:\\temp\\Intellicare\\",
 				outputFileName = fileName.replaceAll("\\.(\\w+)$", "_" + testMethodName + ".$1");
 		
@@ -70,20 +74,19 @@ public class ImageExtractorTest extends TestCase {
 	public void testGetImageListIsNotNullOrEmpty() throws Exception {
 		// * input params *
 		String testMethodName = methodName();
-		String   baseUrl = "http://cbits.northwestern.edu/images/"
-				,fileName = "cbitsfulllogo.png"
-				,url = baseUrl + fileName;	// Yahoo logo
+		String   baseUrl = "http://cbits.northwestern.edu/"
+				,fileName = ""
+				,url = baseUrl + fileName;
 		String 	outputFolder = "C:\\temp\\Intellicare\\",
 				outputFileName = fileName.replaceAll("\\.(\\w+)$", "_" + testMethodName + ".$1");
 			
 		// * execute *
 		try {
 			// * test *
-			log(testMethodName, "Fetching image from: " + url);
-			byte[] image = ImageExtractor.getImage(url);
-			assertNotNull(image);
-			// let's write the image so we can easily view it: http://stackoverflow.com/questions/12465586/how-can-i-download-an-image-using-jsoup
-			saveImage(testMethodName, image, outputFolder + outputFileName);
+			log(testMethodName, "Fetching page from: " + url);
+			ArrayList<String> imageUrlList = ImageExtractor.getImageList(url, false);
+			assertNotNull(imageUrlList);
+			log(testMethodName, "list = " + imageUrlList);
 		} catch (IOException e) {
 			e.printStackTrace();
 			fail("IOException during HTTP(S) call.");
@@ -91,6 +94,69 @@ public class ImageExtractorTest extends TestCase {
 	}
 
 	
+	
+//	.replaceAll("([\\w-_\\+\\='\"\\?,\\.%\\&:]+)\\.(\\w+)$", testMethodName + "_" + "$1" + ".$2");
+
+	public void testListAndDownloadImages() throws Exception {
+		// * input params *
+		String testMethodName = methodName();
+		String   baseUrl = "http://cbits.northwestern.edu/"
+				,fileName = ""
+				,url = baseUrl + fileName;
+		String 	outputFolder = "C:\\temp\\Intellicare\\";
+		
+		// * execute *
+		try {
+			// * test *
+			log(testMethodName, "Fetching page from: " + url);
+			ArrayList<String> imageUrlList = ImageExtractor.getImageList(url, false);
+			log(testMethodName, "list = " + imageUrlList);
+			downloadAndSaveImages(outputFolder, imageUrlList);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("IOException during HTTP(S) call.");
+		}
+	}
+	
+	public void testListAndDownloadImagesFromMultipleUrls() throws Exception {
+		String testMethodName = methodName();
+
+		ArrayList<String> urls = new ArrayList<String>();
+		urls.add("http://www.flickr.com/explore");
+		urls.add("http://www.flickr.com/map");
+		urls.add("http://www.flickr.com/galleries");
+		urls.add("https://www.google.com/search?safe=active&site=&tbm=isch&source=hp&biw=1547&bih=927&q=kittens&oq=kittens&gs_l=img.3..0l10.1111.1821.0.1901.7.7.0.0.0.0.111.483.6j1.7.0....0...1ac.1.34.img..1.6.394.tsrj5JwqEPo");
+		urls.add("http://icanhas.cheezburger.com/lolcats");
+		urls.add("http://pixabay.com/en/");
+		urls.add("http://www.publicdomainpictures.net/");
+		urls.add("http://addicted2success.com/quotes/images-56-inspirational-picture-quotes-that-will-motivate-your-mind/");
+		urls.add("http://www.pinterest.com/justcoachit/inspiring-pics-quotes/");
+		urls.add("http://mashable.com/2012/12/08/inspiring-photos-2012/");
+
+		String 	baseOutputFolder = "C:\\temp\\Intellicare\\";
+
+		// * execute *
+		try {
+			// * test *
+			for(String u : urls) {
+				log(testMethodName, "Fetching page from: " + u);
+//				ArrayList<String> imageUrlList = ImageExtractor.getImageList(u, false);
+				String outputFolder = baseOutputFolder + extractFQDN(u) + "\\";
+				log(testMethodName, "outputFolder = " + outputFolder); // + " ; list = " + imageUrlList);
+//				downloadAndSaveImages(outputFolder, imageUrlList);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("IOException during HTTP(S) call.");
+		}
+	}
+	
+	public static String extractFQDN(String url) {
+		return url.replace("(?i)https{0,1}://(.*)", "$1"); //.replace("^(.*?)/.*$", "$1");
+	}
+	
+	
+
 	/***** test utility functions *****/
 	
 	private static void log(String fn, String msg) {
@@ -102,6 +168,26 @@ public class ImageExtractorTest extends TestCase {
 		log(testMethodName, "Writing image to: " + fullFilePath);
         out.write(image);           // resultImageResponse.body() is where the image's contents are.
         out.close();
+	}
+	
+	/**
+	 * @param testMethodName
+	 * @param outputFolder
+	 * @param imageUrlList
+	 */
+	private void downloadAndSaveImages(String outputFolder, ArrayList<String> imageUrlList) {
+		String testMethodName = methodName();
+		
+		// download and write the files
+		for(String u : imageUrlList) {
+			String outputFileName = u.replaceAll("^.*/(.*)\\??.*$", "$1");
+			log(testMethodName, "outputFileName = " + outputFileName + "; u = " + u);
+			try{
+				saveImage(testMethodName, ImageExtractor.getImage(u), outputFolder + outputFileName);
+			} catch(Exception e) {
+				log(testMethodName, "ERROR: Couldn't write file at: " + outputFileName + " from URL (" + u + "). Reason: " + e.getMessage());
+			}
+		}
 	}
 	
 	
