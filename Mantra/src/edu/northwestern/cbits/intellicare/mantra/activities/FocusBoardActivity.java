17b01cc61a09eb6a2f8d2e60fb4c3660a0f46484
Evan Story <estory1@gmail.com>
2014-02-07 10:50:34 -0600

    Enable image browsing using Android's built-in image browser.

--- Mantra/src/edu/northwestern/cbits/intellicare/mantra/activities/FocusBoardActivity.java
+++ Mantra/src/edu/northwestern/cbits/intellicare/mantra/activities/FocusBoardActivity.java
@@ -6,11 +6,15 @@ import edu.northwestern.cbits.intellicare.mantra.FocusBoardManager;
 import edu.northwestern.cbits.intellicare.mantra.FocusImageGridFragment;
 import edu.northwestern.cbits.intellicare.mantra.R;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -21,6 +25,9 @@ public class FocusBoardActivity extends ActionBarActivity {
 	private FocusBoardManager mManager;
 	private long mFocusBoardId;
 
+	private static int RESULT_LOAD_IMAGE = 1;
+
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -46,10 +53,45 @@ public class FocusBoardActivity extends ActionBarActivity {
 		case R.id.new_image_action:
 			startCollectCameraActivity();
 			return true;
+		case R.id.existing_image_action:
+			startBrowsePhotosActivity();
+			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
 	}
+
+	/* display image from gallery: BEGIN (src: http://viralpatel.net/blogs/pick-image-from-galary-android-app/) */
+	private void startBrowsePhotosActivity() {
+		Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		startActivityForResult(i, RESULT_LOAD_IMAGE);
+	}
+
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+        Log.d("onActivityResult", "entered; requestCode = " + requestCode + "; resultCode = " + resultCode + "; data = " + data);
+
+        // if: existing image
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+	         Uri selectedImage = data.getData();
+	         String[] filePathColumn = { MediaStore.Images.Media.DATA };
+	 
+	         Cursor cursor = getContentResolver().query(selectedImage,
+	                 filePathColumn, null, null, null);
+	         cursor.moveToFirst();
+	 
+	         int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+	         String picturePath = cursor.getString(columnIndex);
+	         
+	         cursor.close();
+	                      
+	         // String picturePath contains the path of selected Image
+	         Log.d("onActivityResult", "picturePath = " + picturePath);
+	     }
+	}
+	/* display image from gallery: END (src: http://viralpatel.net/blogs/pick-image-from-galary-android-app/) */
 
 	private void startCollectCameraActivity() {
 		Intent intent = new Intent(this, CollectCameraActivity.class);
