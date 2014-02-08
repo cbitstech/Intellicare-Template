62836181443259c0a12c7f25f8d33a3b1c436726
Evan Story <estory1@gmail.com>
2014-02-07 20:16:49 -0600

    * Added intent-handler to Mantra. FocusBoardListActivity receives and can successfully get the URL sent by e.g. Chrome during a Share -> Focus Boards operation. * But, this currently throws an exception on the Jsoup.connect(url) call in the ImageExtractor.getImageList(...) method.

--- Mantra/src/edu/northwestern/cbits/intellicare/mantra/activities/FocusBoardListActivity.java
+++ Mantra/src/edu/northwestern/cbits/intellicare/mantra/activities/FocusBoardListActivity.java
@@ -1,12 +1,18 @@
 package edu.northwestern.cbits.intellicare.mantra.activities;
 
+import java.io.IOException;
+import java.util.Set;
+
 import edu.northwestern.cbits.intellicare.mantra.DatabaseHelper;
 import edu.northwestern.cbits.intellicare.mantra.FocusBoardCursorAdapter;
 import edu.northwestern.cbits.intellicare.mantra.FocusBoardManager;
 import edu.northwestern.cbits.intellicare.mantra.DatabaseHelper.FocusBoardCursor;
+import edu.northwestern.cbits.intellicare.mantra.ImageExtractor;
 import edu.northwestern.cbits.intellicare.mantra.R;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class FocusBoardListActivity extends ListActivity {
 	
@@ -19,8 +25,35 @@ public class FocusBoardListActivity extends ListActivity {
 		mCursor = FocusBoardManager.get(this).queryFocusBoards();
 		FocusBoardCursorAdapter adapter = new FocusBoardCursorAdapter(this, mCursor);
 		setListAdapter(adapter);
+		
+		
+		// handle external intents; src: http://developer.android.com/training/sharing/receive.html
+		Intent intent = getIntent();
+		String action = intent.getAction();
+		String type = intent.getType();
+		
+		Log.d("FocusBoardActivityList.onCreate", "action = " + action + "; type = " + type);
+		if(Intent.ACTION_SEND.equals(action) && type != null) {
+			if("text/plain".equals(type)) {
+				handleSendText(intent);
+			}
+		}
 	}
 	
+	private void handleSendText(Intent intent) {
+		Log.d("FocusBoardActivityList.handleSendText", "entered; intent = " + intent);
+
+		try {
+			Set<String> imageSet = ImageExtractor.getImageList(intent.getStringExtra(Intent.EXTRA_TEXT), false);
+			for(String s : imageSet) {
+				Log.d("FocusBoardActivityList.handleSendText", "image URL = " + s);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	protected void onDestroy() {
 		mCursor.close();
