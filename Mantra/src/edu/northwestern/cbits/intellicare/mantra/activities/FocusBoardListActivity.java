d18d9552078ab387058de2e7b3f8d355fef73503
Evan Story <estory1@gmail.com>
2014-02-12 13:43:33 -0600

    * Added Stephen's list of pre-compiled mantras. * Renamed "lib" to "libs", so Android sees the JSoup library. * Added the Internet permission. * Successfully getting image URLs and image sizes.

--- Mantra/src/edu/northwestern/cbits/intellicare/mantra/activities/FocusBoardListActivity.java
+++ Mantra/src/edu/northwestern/cbits/intellicare/mantra/activities/FocusBoardListActivity.java
@@ -1,6 +1,8 @@
 package edu.northwestern.cbits.intellicare.mantra.activities;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import edu.northwestern.cbits.intellicare.mantra.DatabaseHelper;
@@ -11,6 +13,7 @@ import edu.northwestern.cbits.intellicare.mantra.ImageExtractor;
 import edu.northwestern.cbits.intellicare.mantra.R;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -43,15 +46,11 @@ public class FocusBoardListActivity extends ListActivity {
 	private void handleSendText(Intent intent) {
 		Log.d("FocusBoardActivityList.handleSendText", "entered; intent = " + intent);
 
-		try {
-			Set<String> imageSet = ImageExtractor.getImageList(intent.getStringExtra(Intent.EXTRA_TEXT), false);
-			for(String s : imageSet) {
-				Log.d("FocusBoardActivityList.handleSendText", "image URL = " + s);
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//			Set<String> imageSet = ImageExtractor.getImageList(intent.getStringExtra(Intent.EXTRA_TEXT), false);
+//			for(String s : imageSet) {
+//				Log.d("FocusBoardActivityList.handleSendText", "image URL = " + s);
+		new GetImageListTask().execute(intent.getStringExtra(Intent.EXTRA_TEXT));
+//			}
 	}
 
 	@Override
@@ -60,3 +59,53 @@ public class FocusBoardListActivity extends ListActivity {
 		super.onDestroy();
 	}
 }
+
+
+
+/**** Async tasks *****/
+
+/**
+ * Fetches the set of image URLs from webpage at a specified URL.
+ * @author mohrlab
+ *
+ */
+class GetImageListTask extends AsyncTask<String, Void, Set<String>> {
+	@Override
+	protected Set<String> doInBackground(String... arg0) {
+		try {
+			return ImageExtractor.getImageList(arg0[0], false);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	protected void onPostExecute(Set<String> imageSet) {
+//		for(String s : imageSet) {
+//			Log.d("FocusBoardActivityList.handleSendText", "image URL = " + s);
+//		}
+		new GetRemoteContentLengthTask().execute(imageSet);
+	}
+}
+
+/**
+ * Fetches the size of images at a specified set of URLs.
+ * @author mohrlab
+ *
+ */
+class GetRemoteContentLengthTask extends AsyncTask<Set<String>, Void, Map<String, Integer>> {
+	
+	protected void onPostExecute(Map<String, Integer> imageSizes) {
+		for(String key : imageSizes.keySet()) {
+			Log.d("FocusBoardActivityList.handleSendText", "size = " + imageSizes.get(key) + " for image " + key);
+		}		
+	}
+
+	@Override
+	protected Map<String, Integer> doInBackground(Set<String>... params) {
+		return ImageExtractor.getRemoteContentLength(params[0]);
+	}
+}
+
+
+
