c708b5fe3fa65576026724f4933f7c65624c393d
Chris Karr <github@audacious-software.com>
2014-03-04 20:29:28 -0600

    Added reminders.

--- Slumber Time/src/edu/northwestern/cbits/intellicare/slumbertime/SettingsActivity.java
+++ Slumber Time/src/edu/northwestern/cbits/intellicare/slumbertime/SettingsActivity.java
@@ -2,10 +2,17 @@ package edu.northwestern.cbits.intellicare.slumbertime;
 
 import java.util.HashMap;
 
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
 import edu.northwestern.cbits.intellicare.ConsentedActivity;
 import edu.northwestern.cbits.intellicare.logging.LogManager;
 
@@ -45,6 +52,36 @@ public class SettingsActivity extends PreferenceActivity
 		if (key == null)
 		{
 			
+		}
+		else if (key.equals("settings_reminder_time"))
+		{
+			final SettingsActivity me = this;
+			
+			final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			
+			TimePickerDialog dialog = new TimePickerDialog(this, new OnTimeSetListener()
+			{
+				public void onTimeSet(TimePicker arg0, int hour, int minute) 
+				{
+			        Editor editor = prefs.edit();
+			        
+			        editor.putInt(AlarmService.REMINDER_HOUR, hour);
+			        editor.putInt(AlarmService.REMINDER_MINUTE, minute);
+			        editor.commit();
+			        
+					HashMap<String, Object> payload = new HashMap<String, Object>();
+					payload.put("hour", hour);
+					payload.put("minute", minute);
+					payload.put("source", "settings");
+					
+					payload.put("full_mode", prefs.getBoolean("settings_full_mode", true));
+					LogManager.getInstance(me).log("set_reminder_time", payload);
+				}
+			}, prefs.getInt(AlarmService.REMINDER_HOUR, 9), prefs.getInt(AlarmService.REMINDER_MINUTE, 0), DateFormat.is24HourFormat(this));
+			
+			dialog.show();
+
+			return true;
 		}
 		else if (key.equals("copyright_statement"))
 			ConsentedActivity.showCopyrightDialog(this);
