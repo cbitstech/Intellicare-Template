957b34a99458ea5c55d686c1f510f933385c3221
Chris Karr <github@audacious-software.com>
2014-03-08 20:36:00 -0600

    Further Foursquare work

--- Avast Ye Hearties/src/edu/northwestern/cbits/intellicare/avast/AvastContentProvider.java
+++ Avast Ye Hearties/src/edu/northwestern/cbits/intellicare/avast/AvastContentProvider.java
@@ -21,9 +21,11 @@ import android.app.Activity;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.database.Cursor;
-import android.location.Location;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class AvastContentProvider extends ContentProvider 
@@ -35,7 +37,17 @@ public class AvastContentProvider extends ContentProvider
 		String foursquareId = null;
 	}
 
+	public static class Category
+	{
+		String name = null;
+		String id = null;
+		String parentId = null;
+	}
+
+	protected static final String SAVED_CATEGORIES = "cache_saved_categories";
+
 	protected static ArrayList<Venue> _lastVenues;
+	protected static ArrayList<Category> _lastCategories;
 
 	@Override
 	public int delete(Uri arg0, String arg1, String[] arg2) {
@@ -81,10 +93,10 @@ public class AvastContentProvider extends ContentProvider
 		{
 			public void run() 
 			{
-				String query = "https://api.foursquare.com/v2/venues/search?client_id=" + context.getString(R.string.fs_client_id) + 
-							 "&client_secret=" + context.getString(R.string.fs_client_secret) + "&v=20130815&ll=" + target.latitude + "," + target.longitude +
-							 "&categoryId=" + category;
-								
+				String query = context.getString(R.string.query_venue, context.getString(R.string.fs_client_id), context.getString(R.string.fs_client_secret), target.latitude, target.longitude, category);
+
+				Log.e("AYH", "FETCH LOCATIONS FROM " + query);
+				
 				try 
 				{
 					URL url = new URL(query);
@@ -99,11 +111,11 @@ public class AvastContentProvider extends ContentProvider
 
 					String body = IOUtils.toString(in, encoding);
 
+					Log.e("AYH", "FETCHED LOCATIONS FROM " + query);
+
 					JSONObject json = new JSONObject(body);
 					JSONObject response = json.getJSONObject("response");
 					JSONArray venues = response.getJSONArray("venues");
-					
-					Log.e("AYH", "JSON: " + json.toString(2));
 					
 					AvastContentProvider._lastVenues = new ArrayList<Venue>();
 					
@@ -152,5 +164,121 @@ public class AvastContentProvider extends ContentProvider
 	public static List<Venue> getLastLocations() 
 	{
 		return AvastContentProvider._lastVenues;
+	}
+
+	public static void fetchCategories(final Context context, final Runnable callback) 
+	{
+		if (AvastContentProvider._lastCategories != null)
+			callback.run();
+		
+		Runnable r = new Runnable()
+		{
+			public void run() 
+			{
+				String query = context.getString(R.string.query_categories, context.getString(R.string.fs_client_id), context.getString(R.string.fs_client_secret));
+
+				try 
+				{
+					SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+					
+					String savedCategories = prefs.getString(AvastContentProvider.SAVED_CATEGORIES, null);
+					
+					if (savedCategories == null)
+					{
+						URL url = new URL(query);
+	
+						URLConnection con = url.openConnection();
+						
+						InputStream in = con.getInputStream();
+						
+						String encoding = con.getContentEncoding();
+						
+						encoding = encoding == null ? "UTF-8" : encoding;
+	
+						savedCategories = IOUtils.toString(in, encoding);
+					}
+
+					JSONObject json = new JSONObject(savedCategories);
+					JSONObject response = json.getJSONObject("response");
+					JSONArray categories = response.getJSONArray("categories");
+					
+					AvastContentProvider._lastCategories = new ArrayList<Category>();
+
+					for (int i = 0; i < categories.length(); i++)
+					{
+						JSONObject category = categories.getJSONObject(i);
+						
+						List<Category> foundCategories = AvastContentProvider.parseCategories(category, null);
+						
+						AvastContentProvider._lastCategories.addAll(foundCategories);
+					}
+					
+					Editor e = prefs.edit();
+					e.putString(AvastContentProvider.SAVED_CATEGORIES, savedCategories);
+					e.commit();
+					
+					if (context instanceof Activity)
+					{
+						Activity a = (Activity) context;
+						
+						a.runOnUiThread(callback);
+					}
+					else
+						callback.run();
+				} 
+				catch (MalformedURLException e) 
+				{
+					LogManager.getInstance(context).logException(e);
+				} 
+				catch (IOException e) 
+				{
+					LogManager.getInstance(context).logException(e);
+				} 
+				catch (JSONException e) 
+				{
+					LogManager.getInstance(context).logException(e);
+				}
+			}
+		};
+		
+		Thread t = new Thread(r);
+		t.start();
+	}
+
+	protected static List<Category> parseCategories(JSONObject category, String parent) throws JSONException 
+	{
+		ArrayList<Category> categoryList = new ArrayList<Category>();
+		
+		Category c = new Category();
+		
+		if (parent != null)
+			c.name = parent + " » " + category.getString("name");
+		else
+			c.name = category.getString("name");
+		
+		c.id = category.getString("id");
+		
+		categoryList.add(c);
+		
+		if (category.has("categories"))
+		{
+			JSONArray subcategories = category.getJSONArray("categories");
+			
+			for (int i = 0; i < subcategories.length(); i++)
+			{
+				JSONObject subcategory = subcategories.getJSONObject(i);
+				
+				List<Category> subs = AvastContentProvider.parseCategories(subcategory, c.name);
+				
+				categoryList.addAll(subs);
+			}
+		}
+		
+		return categoryList;
+	}
+
+	public static List<Category> getLastCategories()
+	{
+		return AvastContentProvider._lastCategories;
 	}
 }
--- Avast Ye Hearties/src/edu/northwestern/cbits/intellicare/avast/MainActivity.java
+++ Avast Ye Hearties/src/edu/northwestern/cbits/intellicare/avast/MainActivity.java
@@ -1,14 +1,7 @@
 package edu.northwestern.cbits.intellicare.avast;
 
+import java.util.HashSet;
 import java.util.List;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import edu.northwestern.cbits.intellicare.avast.AvastContentProvider.Venue;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -18,12 +11,31 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ListView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import edu.northwestern.cbits.intellicare.avast.AvastContentProvider.Category;
+import edu.northwestern.cbits.intellicare.avast.AvastContentProvider.Venue;
 
 public class MainActivity extends ActionBarActivity 
 {
 	private static final String LAST_LATITUDE = "last_known_latitude";
 	private static final String LAST_LONGITUDE = "last_known_longitude";
+	
+	private final HashSet<String> _selectedIds = new HashSet<String>();
 
 	protected void onCreate(Bundle savedInstanceState) 
 	{
@@ -60,6 +72,69 @@ public class MainActivity extends ActionBarActivity
 		super.onResume();
 		
 		this.redrawMap();
+		
+		final MainActivity me = this;
+		
+		final ListView categories = (ListView) this.findViewById(R.id.categories_list);
+		
+		AvastContentProvider.fetchCategories(this, new Runnable()
+		{
+			public void run() 
+			{
+				List<Category> categoryList = AvastContentProvider.getLastCategories();
+				
+				ArrayAdapter<Category> adapter = new ArrayAdapter<Category>(me, R.layout.row_checkbox, categoryList)
+				{
+					public View getView (int position, View convertView, ViewGroup parent)
+					{
+						if (convertView == null)
+						{
+							LayoutInflater inflater = LayoutInflater.from(me);
+							convertView = inflater.inflate(R.layout.row_checkbox, null, false);
+						}
+						
+						convertView = super.getView(position, convertView, parent);
+						
+						final Category category = this.getItem(position);
+						
+						Log.e("AYH", "GOT: " + category.name);
+						
+						CheckBox check = (CheckBox) convertView.findViewById(R.id.checkbox);
+
+						check.setOnCheckedChangeListener(new OnCheckedChangeListener()
+						{
+							public void onCheckedChanged(CompoundButton arg0, boolean checked) 
+							{
+
+							}
+						});
+
+						check.setChecked(me._selectedIds.contains(category.id));
+						
+						check.setOnCheckedChangeListener(new OnCheckedChangeListener()
+						{
+							public void onCheckedChanged(CompoundButton arg0, boolean checked) 
+							{
+								if (checked)
+									me._selectedIds.add(category.id);
+								else
+									me._selectedIds.remove(category.id);
+								
+								me.redrawMap();
+							}
+						});
+						
+						check.setText(category.name);
+						
+						return convertView;
+					}
+				};
+				
+				categories.setAdapter(adapter);
+				adapter.notifyDataSetChanged();
+				categories.invalidate();
+			}
+		});
 	}
 	
 	private void redrawMap()
@@ -67,25 +142,40 @@ public class MainActivity extends ActionBarActivity
 		MapFragment fragment = (MapFragment) this.getFragmentManager().findFragmentById(R.id.map);
 		
 		final GoogleMap map = fragment.getMap();
-
-		AvastContentProvider.fetchLocations(this, map.getCameraPosition().target, "4bf58dd8d48988d17f941735", new Runnable()
+		
+		StringBuffer sb = new StringBuffer();
+		
+		for (String selected : this._selectedIds)
 		{
-			public void run() 
+			if (sb.length() > 0)
+				sb.append(",");
+			
+			sb.append(selected);
+		}
+		
+		Log.e("AYH", "LOC QUERY: " + sb.toString());
+		
+		if (sb.length() > 0)
+		{		
+			AvastContentProvider.fetchLocations(this, map.getCameraPosition().target, sb.toString(), new Runnable()
 			{
-				map.clear();
-
-				for (Venue venue : AvastContentProvider.getLastLocations())
+				public void run() 
 				{
-					MarkerOptions marker = new MarkerOptions();
-					marker.position(venue.location);
-					marker.title(venue.name);
-					
-					Log.e("AYH", "MARKER: " + marker);
-					
-					map.addMarker(marker);
+					map.clear();
+	
+					for (Venue venue : AvastContentProvider.getLastLocations())
+					{
+						MarkerOptions marker = new MarkerOptions();
+						marker.position(venue.location);
+						marker.title(venue.name);
+						
+						map.addMarker(marker);
+					}
 				}
-			}
-		}); // Movie Theatre
+			});
+		}
+		else
+			map.clear();
 	}
 
 	public boolean onCreateOptionsMenu(Menu menu) 
