cef5cadc7042267c5944ae69ff021050218cd87c
Evan Story <estory1@gmail.com>
2014-03-10 14:04:40 -0500

    * A bit of code cleanup.

--- Mantra/src/edu/northwestern/cbits/intellicare/mantra/Constants.java
+++ Mantra/src/edu/northwestern/cbits/intellicare/mantra/Constants.java
@@ -4,4 +4,12 @@ public final class Constants {
 	
 	public static final String BROADCAST_ACTION = "edu.northwestern.cbits.intellicare.mantra.BROADCAST";
 	public static final String BROADCAST_STATUS = "edu.northwestern.cbits.intellicare.mantra.STATUS";
+	
+	public static final String REMINDER_START_HOUR = "start_hour";
+	public static final String REMINDER_START_MINUTE = "start_minute";
+	public static final String REMINDER_END_HOUR = "end_hour";
+	public static final String REMINDER_END_MINUTE = "end_minute";
+	public static final int DEFAULT_HOUR = 18;
+	public static final int DEFAULT_MINUTE = 0;
+	private static final String LAST_NOTIFICATION = "last_notification";
 }
