2320d4d4371be1e74268383dc33fbd28a5a30092
Chris Karr <github@audacious-software.com>
2014-04-21 04:06:14 -0500

    Incorporated view pager for HTML content.

--- IntellicareTemplate/src/edu/northwestern/cbits/intellicare/SequentialPageActivity.java
+++ IntellicareTemplate/src/edu/northwestern/cbits/intellicare/SequentialPageActivity.java
@@ -1,25 +1,27 @@
 package edu.northwestern.cbits.intellicare;
 
-import java.util.HashMap;
-
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.webkit.WebView;
-import android.widget.Button;
 import edu.northwestern.cbits.ic_template.R;
-import edu.northwestern.cbits.intellicare.logging.LogManager;
 
 public abstract class SequentialPageActivity extends ConsentedActivity 
 {
-	private static final String INITIAL_PAGE = "INITIAL_PAGE";
-
 	public abstract int pagesSequence();
 	public abstract int titlesSequence();
 	
-	private int _pageIndex = 0;
 	private String[] _urls = null;
 	private String[] _titles = null;
+	private String[] _subtitles = null;
+	
+	private Menu _menu = null;
 	
 	protected void onCreate(Bundle savedInstanceState) 
 	{
@@ -27,80 +29,178 @@ public abstract class SequentialPageActivity extends ConsentedActivity
 
 		this.setContentView(R.layout.activity_sequential);
 
-		this._pageIndex = this.getIntent().getIntExtra(SequentialPageActivity.INITIAL_PAGE, 0);
-
 		this._urls = this.getResources().getStringArray(this.pagesSequence());
 		this._titles = this.getResources().getStringArray(this.titlesSequence());
-	}
-	
-	public void onResume()
-	{
-		super.onResume();
 		
-		this.showPage();
-	}
-	
-	public void showPage()
-	{
-		String url = this._urls[this._pageIndex];
-		String title = this._titles[this._pageIndex];
-		
-		this.getSupportActionBar().setTitle(title);
-		
-		final SequentialPageActivity me = this;
+		if (this.subtitlesSequence() != -1)
+			this._subtitles = this.getResources().getStringArray(this.subtitlesSequence());
 
-		WebView webView = (WebView) this.findViewById(R.id.web_view);
-		webView.loadUrl(url);
-		
-		HashMap<String, Object> payload = new HashMap<String, Object>();
-		payload.put("page_url", url);
-		payload.put("timestamp", Long.valueOf(System.currentTimeMillis()));
-		
-		LogManager.getInstance(this).log("page_displayed", payload);
-		
-		Button back = (Button) this.findViewById(R.id.back_button);
-		
-		if (this._pageIndex == 0)
-			back.setVisibility(View.INVISIBLE);
-		else
-			back.setVisibility(View.VISIBLE);
-		
-		back.setOnClickListener(new OnClickListener()
+        final ViewPager pager = (ViewPager) this.findViewById(R.id.pager_content);
+        pager.setOffscreenPageLimit(0);
+        
+        final SequentialPageActivity me = this;
+        
+		PagerAdapter adapter = new PagerAdapter()
 		{
-			public void onClick(View view) 
+			public int getCount() 
 			{
-				me._pageIndex -= 1;
-				me.showPage();
+				return me._urls.length;
 			}
-		});
-		
-		Button next = (Button) this.findViewById(R.id.next_button);
 
-		if (this._pageIndex == this._urls.length - 1)
-			next.setText(R.string.button_close);
-		else
-			next.setText(R.string.button_next);
-
-		next.setOnClickListener(new OnClickListener()
-		{
-			public void onClick(View view) 
+			public boolean isViewFromObject(View view, Object content) 
 			{
-				if (me._pageIndex < me._urls.length - 1)
+				return view.getTag().equals(content);
+			}
+			
+			public void destroyItem (ViewGroup container, int position, Object content)
+			{
+				int toRemove = -1;
+				
+				for (int i = 0; i < container.getChildCount(); i++)
 				{
-					me._pageIndex += 1;
-					me.showPage();
+					View child = container.getChildAt(i);
+					
+					if (this.isViewFromObject(child, content))
+						toRemove = i;
 				}
+				
+				if (toRemove >= 0)
+					container.removeViewAt(toRemove);
+			}
+			
+			public Object instantiateItem (ViewGroup container, int position)
+			{
+				WebView webView = new WebView(container.getContext());
+
+				webView.loadUrl(me._urls[position]);
+				
+				webView.setTag("" + position);
+
+				container.addView(webView);
+
+				LayoutParams layout = (LayoutParams) webView.getLayoutParams();
+				layout.height = LayoutParams.MATCH_PARENT;
+				layout.width = LayoutParams.MATCH_PARENT;
+				
+				webView.setLayoutParams(layout);
+
+				return webView.getTag();
+			}
+		};
+		
+		pager.setAdapter(adapter);
+		pager.setOnPageChangeListener(new OnPageChangeListener()
+		{
+			public void onPageScrollStateChanged(int arg0) 
+			{
+
+			}
+
+			public void onPageScrolled(int arg0, float arg1, int arg2) 
+			{
+
+			}
+
+			public void onPageSelected(int page) 
+			{
+				me.getActionBar().setTitle(me._titles[page]);
+				
+				if (me._subtitles != null)
+					me.getActionBar().setSubtitle(me._subtitles[page]);
 				else
+					me.getActionBar().setSubtitle(me.getString(R.string.subtitle_pages, (page + 1), me._titles.length));
+				
+				if (me._menu != null)
 				{
-					me.onSequenceComplete();
-					me.finish();
+					MenuItem nextItem = me._menu.findItem(R.id.action_next);
+					MenuItem backItem = me._menu.findItem(R.id.action_back);
+					MenuItem doneItem = me._menu.findItem(R.id.action_done);
+
+					switch(page)
+					{
+						case 0:
+							nextItem.setVisible(true);
+							backItem.setVisible(false);
+							doneItem.setVisible(false);
+							break;
+						default:
+							if (page == me._urls.length - 1)
+							{
+								nextItem.setVisible(false);
+								backItem.setVisible(true);
+								doneItem.setVisible(true);
+							}
+							else
+							{
+								nextItem.setVisible(true);
+								backItem.setVisible(true);
+								doneItem.setVisible(false);
+							}
+							
+							break;
+					}
 				}
 			}
 		});
+		
+		this.getActionBar().setTitle(this._titles[0]);
+		
+		if (this._subtitles != null)
+			this.getActionBar().setSubtitle(this._subtitles[0]);
+		else
+			this.getActionBar().setSubtitle(this.getString(R.string.subtitle_pages, 1, this._titles.length));
 	}
 
 	public void onSequenceComplete()
 	{
 		// Placeholder for sequences with no completion action.
 	}
+	
+	public int subtitlesSequence() 
+	{
+		return -1;
+	}
+
+    public boolean onCreateOptionsMenu(Menu menu) 
+    {
+        this.getMenuInflater().inflate(R.menu.menu_sequential, menu);
+        
+        this._menu = menu;
+
+		MenuItem backItem = this._menu.findItem(R.id.action_back);
+		MenuItem doneItem = this._menu.findItem(R.id.action_done);
+
+		backItem.setVisible(false);
+		doneItem.setVisible(false);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) 
+    {
+    	ViewPager pager = (ViewPager) this.findViewById(R.id.pager_content);
+    	
+    	if (item.getItemId() == R.id.action_next)
+    	{
+    		pager.setCurrentItem(pager.getCurrentItem() + 1);
+    		
+    		return true;
+    	}
+    	else if (item.getItemId() == R.id.action_back)
+    	{
+    		pager.setCurrentItem(pager.getCurrentItem() - 1);
+    		
+    		return true;
+    	}
+    	else if (item.getItemId() == R.id.action_done)
+    	{
+			this.onSequenceComplete();
+    		this.finish();
+    		
+    		return true;
+    	}
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
