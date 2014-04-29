01a9ce19d877b3434b3c48335abdde87b339df9b
gwensmu <gwen.smuda@gmail.com>
2014-04-29 16:33:12 -0500

    Adding Gratitude for Remote Work

---
+++ Gratitude/gratitude/src/main/java/edu/northwestern/cbits/intellicare/gratitude/NaikanEntryActivity.java
@@ -0,0 +1,9 @@
+package edu.northwestern.cbits.intellicare.relax.gratitude.gratitude;
+
+import android.app.Activity;
+
+/**
+ * Created by Gwen on 4/14/2014.
+ */
+public class NaikanEntryActivity extends Activity {
+}
