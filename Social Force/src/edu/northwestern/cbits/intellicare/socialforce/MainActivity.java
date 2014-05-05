db00e1521e60433282208d51845ae83e23d8a2f6
Chris Karr <github@audacious-software.com>
2014-05-05 09:30:35 -0500

    Graph work (in progress).

--- Social Force/src/edu/northwestern/cbits/intellicare/socialforce/MainActivity.java
+++ Social Force/src/edu/northwestern/cbits/intellicare/socialforce/MainActivity.java
@@ -1,21 +1,35 @@
 package edu.northwestern.cbits.intellicare.socialforce;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.WebView;
 import edu.northwestern.cbits.intellicare.ConsentedActivity;
+import edu.northwestern.cbits.intellicare.logging.LogManager;
 
 public class MainActivity extends ConsentedActivity 
 {
@@ -102,9 +116,127 @@ public class MainActivity extends ConsentedActivity
 	        Intent introIntent = new Intent(this, RatingActivity.class);
 	        this.startActivity(introIntent);
         }
+        
+        this.refreshBubbles();
     }
 
-    public boolean onCreateOptionsMenu(Menu menu) 
+    @SuppressLint("SetJavaScriptEnabled")
+	private void refreshBubbles() 
+    {
+		WebView graphView = (WebView) this.findViewById(R.id.network_visualization);
+		graphView.getSettings().setJavaScriptEnabled(true);
+		graphView.getSettings().setBuiltInZoomControls(true);
+		graphView.getSettings().setLoadWithOverviewMode(true);
+		graphView.getSettings().setUseWideViewPort(true);
+		graphView.setInitialScale(1);
+		
+		graphView.loadDataWithBaseURL("file:///android_asset/viz/", MainActivity.generateBubbles(this), "text/html", null, null);
+	}
+
+	private static String generateBubbles(Context context) 
+	{
+	    StringBuilder buffer = new StringBuilder();
+	    
+		try 
+		{
+		    InputStream html = context.getAssets().open("viz/home_bubbles.html");
+
+		    BufferedReader in = new BufferedReader(new InputStreamReader(html));
+
+		    String str = null;
+
+		    while ((str = in.readLine()) != null) 
+		    {
+		    	buffer.append(str);
+		    	buffer.append(System.getProperty("line.separator"));
+		    }
+
+		    in.close();
+		} 
+		catch (IOException e) 
+		{
+			LogManager.getInstance(context).logException(e);
+		}
+
+		String graphString = buffer.toString();
+
+		try 
+		{
+			JSONObject graphValues = MainActivity.bubbleValues(context);
+
+			graphString = graphString.replaceAll("VALUES_JSON", graphValues.toString());
+		} 
+		catch (JSONException e) 
+		{
+			LogManager.getInstance(context).logException(e);
+		} 
+
+		Log.e("SF", "RET: " + graphString);
+		
+		return graphString;
+	}
+
+	private static JSONObject bubbleValues(Context context) throws JSONException 
+	{
+		/*
+		 * 
+		 * var root = {
+  "name": "contacts",
+  "children": [
+    {"name": "AgglomerativeCluster", "size": 3938},
+    {"name": "CommunityStructure", "size": 3812},
+    {"name": "HierarchicalCluster", "size": 3938},
+    {"name": "MergeEdge", "size": 3938}
+  ]
+};
+		 */
+		
+		JSONObject bubbles = new JSONObject();
+
+		bubbles.put("name", "contacts");
+		
+		JSONArray children = new JSONArray();
+
+		List<ContactRecord> contacts = ContactCalibrationHelper.fetchContactRecords(context);
+		
+		for (ContactRecord contact : contacts)
+		{
+			if (contact.level != -1 && contact.name != null && contact.name.trim().length() > 0)
+			{
+				JSONObject bubble = new JSONObject();
+				bubble.put("name", contact.name);
+				bubble.put("size", contact.count);
+				
+				bubble.put("color", "#808080");
+
+				if (ContactCalibrationHelper.isAdvice(context, contact))
+				{
+					bubble.put("color", "#0099CC");
+				}
+				else if (ContactCalibrationHelper.isCompanion(context, contact))
+				{
+					bubble.put("color", "#9933CC");
+				}
+				else if (ContactCalibrationHelper.isEmotional(context, contact))
+				{
+					bubble.put("color", "#CC0000");
+					
+				}
+				else if (ContactCalibrationHelper.isPractical(context, contact))
+				{
+					bubble.put("color", "#669900");
+				}
+				
+				children.put(bubble);
+			}
+		}
+		
+		bubbles.put("children", children);
+		
+		return bubbles;
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu) 
     {
         this.getMenuInflater().inflate(R.menu.menu_main, menu);
 
