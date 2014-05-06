bf866b129d23b3a3c9068fafcb21bf8d525fbd3b
Chris Karr <github@audacious-software.com>
2014-05-06 13:11:27 -0500

    Code cleanup.

--- Social Force/src/edu/northwestern/cbits/intellicare/socialforce/ScheduleManager.java
+++ Social Force/src/edu/northwestern/cbits/intellicare/socialforce/ScheduleManager.java
@@ -1,7 +1,5 @@
 package edu.northwestern.cbits.intellicare.socialforce;
 
-import java.util.Date;
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
