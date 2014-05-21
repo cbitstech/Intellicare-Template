737bdf4503254750f9d6f36683ec932857700a6a
gwensmu <gwen.smuda@gmail.com>
2014-05-21 17:25:26 -0500

    iCope and Aspire - modified 9-patch card backgrounds, changed filter icon in iCope, uncoalesced cards from main screen

---
+++ Aspire/app/src/main/java/edu/northwestern/cbits/intellicare/aspire/StartupActivity.java
@@ -0,0 +1,34 @@
+package edu.northwestern.cbits.intellicare.aspire;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+public class StartupActivity extends Activity 
+{
+    public static final Uri URI = Uri.parse("intellicare://daily-feats/home");
+
+	protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+        
+		if (prefs.getBoolean(IntroActivity.INTRO_SHOWN, false))
+			this.startActivity(new Intent(this, IntroActivity.class));
+		else
+			this.startActivity(new Intent(this, MainActivity.class));
+		
+		ScheduleManager.getInstance(this);
+    }
+
+    protected void onResume() 
+    {
+        super.onResume();
+
+        this.finish();
+    }
+}
